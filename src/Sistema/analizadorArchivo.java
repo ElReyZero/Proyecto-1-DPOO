eb25ba96a3e25266b9d3cda027a1034131f597b3
ElReyZero <juan.romero1201@gmail.com>
2021-03-17 12:00:41 -0500

    Commit

--- src/Sistema/analizadorArchivo.java
+++ src/Sistema/analizadorArchivo.java
@@ -2,4 +2,14 @@ package Sistema;
 
 public class analizadorArchivo {
 
+    private String nombreArchivo;
+    private String ruta;
+
+    public analizadorArchivo(String pNombreArchivo, String pRuta)
+    {
+        nombreArchivo = null;
+        ruta = null;
+    }
+    
+
 }
