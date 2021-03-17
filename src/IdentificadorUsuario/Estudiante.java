ab5ec3bb95ac25f13fd05c73d025931b53f5251d
ElReyZero <juan.romero1201@gmail.com>
2021-03-17 11:18:16 -0500

    Pensum, materia

--- src/IdentificadorUsuario/Estudiante.java
+++ src/IdentificadorUsuario/Estudiante.java
@@ -5,11 +5,12 @@ public class Estudiante extends Usuario {
 
 	private String carrera;
 	private double pga;
-	private HashMap<Integer, HashMap<Materia, Double>>Outer;
+	private HashMap<Integer, HashMap<Materia, Double>> cursosTomados;
+	private HashMap<String, Double> promedioSemestre;
 	
 	public Estudiante(String pNombre, String pCodigo) {
 		super(pNombre, pCodigo);
-		// TODO Auto-generated constructor stub
+		cursosTomados = new HashMap();
 	}
 
 }
