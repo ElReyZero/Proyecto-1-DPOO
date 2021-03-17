8cf935231f9119126e3751bdf60c941c27d12738
luccasrojas <l.rojasb@uniandes.edu.co>
2021-03-17 11:38:44 -0500

    materia

--- src/curriculo/Materia.java
+++ src/curriculo/Materia.java
@@ -1,5 +1,51 @@
 package curriculo;
 
-public class Materia {
+import java.util.ArrayList;
+
+public class Materia
+ {
+    private String nombre;
+    private ArrayList<Materia> preRequisitos = new ArrayList<Materia>();
+    private ArrayList<Materia> requisitos = new ArrayList<Materia>();
+    private int creditos;
+    private String tipoMateria;
+    private boolean semanas16;
+
+    public Materia(String pnombre, ArrayList<Materia> ppreRequisitos, ArrayList<Materia> prequisitos, int pcreditos, String ptipoMateria, boolean psemanas16 )
+
+    {
+        nombre = pnombre;
+        preRequisitos = ppreRequisitos;
+        requisitos = prequisitos;
+        creditos = pcreditos;
+        tipoMateria = ptipoMateria;
+        semanas16 = psemanas16;
+    }
+
+public String darNombre()
+{
+    return nombre;
+}
+public ArrayList<Materia> darPreRequisitos()
+{
+    return preRequisitos;
+}
+public ArrayList<Materia> darRequisitos()
+{
+   return requisitos; 
+}
+public int darCreditos()
+{
+    return creditos;
+}
+public String darTipoMateria()
+{
+    return tipoMateria;
+}
+public boolean darSemanas16()
+{
+    return semanas16;
+}
+
 
 }
