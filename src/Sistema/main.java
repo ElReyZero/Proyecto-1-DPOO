27ee22c7ba64b48f91d203dd5c4aa741ab9bc603
luccasrojas <l.rojasb@uniandes.edu.co>
2021-04-06 09:00:12 -0500

    creacion de usuario

--- src/Sistema/main.java
+++ src/Sistema/main.java
@@ -3,6 +3,9 @@ import java.io.File;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import IdentificadorUsuario.CoordinadorAcademico;
+import IdentificadorUsuario.Estudiante;
+
 public class main 
 {
 
@@ -36,7 +39,7 @@ public class main
                 switch (opcion) {
                     case 1:
                     String nombreEstudiante = "";
-                    String codigo = "";
+                    String codigoEstudiante = "";
                     String carrera = "";
                     System.out.println("\n\n\nBienvenido estudiante \n-----------------------");
                     System.out.println("Digite su nombre:");
@@ -51,7 +54,7 @@ public class main
                     System.out.println("Digite su codigo:");
                     try
                     {
-                        codigo = sn.next();
+                        codigoEstudiante = sn.next();
                     } catch (InputMismatchException e) 
                     {
                         System.out.println("Debes insertar tu codigo");
@@ -66,9 +69,41 @@ public class main
                         System.out.println("Debes insertar el nombre de tu carrera");
                         sn.next();
                     }
+                    Estudiante estudiante = new Estudiante(nombreEstudiante,codigoEstudiante,carrera);
                         break;
                     case 2:
-                        System.out.println("Has seleccionado la opcion 2");
+                    String nombreCoordinador = "";
+                    String codigoCoordinador = "";
+                    String departamento = "";
+                    System.out.println("\n\n\nBienvenido estudiante \n-----------------------");
+                    System.out.println("Digite su nombre:");
+                    try
+                    {
+                        nombreCoordinador = sn.next();
+                    } catch (InputMismatchException e) 
+                    {
+                        System.out.println("Debes insertar tu nombre");
+                        sn.next();
+                    }
+                    System.out.println("Digite su codigo:");
+                    try
+                    {
+                        codigoCoordinador = sn.next();
+                    } catch (InputMismatchException e) 
+                    {
+                        System.out.println("Debes insertar tu codigo");
+                        sn.next();
+                    }
+                    System.out.println("Digite su departamento:");
+                    try
+                    {
+                        departamento = sn.next();
+                    } catch (InputMismatchException e) 
+                    {
+                        System.out.println("Debes insertar tu departamento");
+                        sn.next();
+                    }
+                    CoordinadorAcademico coordinador = new CoordinadorAcademico(nombreCoordinador,codigoCoordinador,departamento);
                         break;
                     case 3:
                         salir = true;
@@ -83,43 +118,7 @@ public class main
             
         }
     }
-	public static void ConsolaCoordinadorAcademico()
-	{
-        Scanner sn = new Scanner(System.in);
-        boolean salir = false;
-        int opcion; 
- 
-        while (!salir) {
-			System.out.println("Bienvenido a Banner \n-----------------------");
-			System.out.println("Elija su tipo de usuario");
- 
-            System.out.println("1. Estudiante");
-            System.out.println("2. Coordinador Académico");
-            System.out.println("3. Salir");
- 
-            try {
- 
-                System.out.println("Escribe una de las opciones: ");
-                opcion = sn.nextInt();
- 
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Has seleccionado la opcion 1");
-                        break;
-                    case 2:
-                        System.out.println("Has seleccionado la opcion 2");
-                        break;
-                    case 3:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("Solo números entre 1 y 3");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Debes insertar un número");
-                sn.next();
-            }
-        }
+	
 	}
-}
+
 
