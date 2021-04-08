46829c18c3b284de7d5fa3f52abeceaa8f27c9d0
ElReyZero <juan.romero1201@gmail.com>
2021-04-08 10:42:05 -0500

    Reciente

--- src/funcionalidades/candidaturaGrado.java
+++ src/funcionalidades/candidaturaGrado.java
@@ -18,16 +18,46 @@ public class candidaturaGrado {
         int cantidadCBPC = 0;
         int cantidadCBCO = 0;
         int cantidadCBCA = 0;
+        int cantidadCBU = 0;
+        int cantidadElectIng = 0;
         for (MateriaEstudiante materia : listacursos) 
         {
             creditosVistos += materia.darCreditos();
+            if(materia.darTipoMateria().equals("Electiva CBU"))
+            {
+                cantidadCBU +=1;
+                if(materia.darTipoMateria().equals("Electiva CBU CO"))
+                {
+                    cantidadCBCO += 1;
+                }
+                else if(materia.darTipoMateria().equals("Electiva CBU CA"))
+                {
+                    cantidadCBCA += 1;
+                }
+                else if(materia.darTipoMateria().equals("Electiva CBU PC"))
+                {
+                    cantidadCBPC +=1;
+                }
+            }
+            else if(materia.darTipoMateria().equals("Electiva Ingeniería"))
+            {
+                cantidadElectIng +=1;
+            }
         }
 
         if(creditosVistos<creditosPensum)
         {
             posible = false;
         }
+        else if(cantidadCBU < 6)
+        {
+            posible = false;
+        }
         else if(cantidadCBCA <1 || cantidadCBCO<1 || cantidadCBPC<1)
+        {
+            posible = false;
+        }
+        else if(cantidadElectIng<1)
         {
             posible = false;
         }
