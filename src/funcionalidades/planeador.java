e34641a3eb0f9146032818cd8d9bcd73f2e214c5
ElReyZero <juan.romero1201@gmail.com>
2021-04-10 14:18:17 -0500

    s

--- src/funcionalidades/planeador.java
+++ src/funcionalidades/planeador.java
@@ -1,26 +1,11 @@
 package funcionalidades;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import IdentificadorUsuario.Estudiante;
 import Sistema.analizadorArchivo;
-import curriculo.Materia;
 import curriculo.Pensum;
 
 public class planeador {
-    //Atributos
-    private ArrayList<Materia> materias;
-    private Integer creditos;
-    private boolean posible;
-
-    //Constructor
-
-    public planeador(ArrayList<Materia> pMaterias, Integer pCreditos)
-    {
-        materias = pMaterias;
-        creditos = pCreditos;
-        posible = false;
-    }
 
     //Métodos
 
@@ -36,19 +21,5 @@ public class planeador {
         return plan;
     }
 
-    public Integer darCreditosPlaneacion()
-    {
-        return creditos;
-    }
-
-    public boolean darPosibilidad()
-    {
-        return posible;
-    }
-
-    public ArrayList<Materia> darMaterias()
-    {
-        return materias;
-    }
 
 }
